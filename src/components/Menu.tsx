81197d7265434f92c5028a27c3fdae3ddfe1427d
Timothy Brennan <2timothy215@gmail.com>
2021-05-06 06:38:47 -0400

    Seperate Menu from Nav into its own component file

---
+++ src/components/Menu.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Menu() {
+  return (
+    <React.Fragment>
+        <div className="hidden lg:flex items-center justify-center antialiased lg:ml-20 pt-1">
+                <a href="#" className="flex items-center justify-center mr-10 text-base text-gray-700 text-opacity-90 font-medium tracking-tight hover:text-cool-gray-600 transition duration-150 ease-in-out">
+                  Product
+                  <svg className="w-3.5 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M19 9l-7 7-7-7"></path>
+                  </svg>
+                </a>
+                <a href="#" className="flex items-center justify-center mr-10 text-base text-gray-700 text-opacity-90 font-medium tracking-tight hover:text-cool-gray-600 transition duration-150 ease-in-out">
+                  Community
+                  <svg className="w-3.5 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M19 9l-7 7-7-7"></path>
+                  </svg>
+                </a>
+                <a href="#" className="flex items-center justify-center mr-10 text-base text-gray-700 text-opacity-90 font-medium tracking-tight hover:text-cool-gray-600 transition duration-150 ease-in-out">
+                  Plans
+                </a>
+              </div>
+    </React.Fragment>
+  );
+}
+
+export default Menu;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Menu from './Menu';
 
 function Nav() {
   return (
     <React.Fragment>
-      <div className="Nav relative bg-white shadow-lg sm:rounded-3xl">        
+      <div className="Nav relative">        
         <div className="px-20 py-6">
           
           <div className="flex items-center justify-between">
@@ -11,23 +12,7 @@ function Nav() {
               <div className="flex items-center justify-center text-3xl font-bold text-true-gray-800">
                 church.
               </div>   
-              <div className="hidden lg:flex items-center justify-center antialiased lg:ml-20 pt-1">
-                <a href="#" className="flex items-center justify-center mr-10 text-base text-gray-700 text-opacity-90 font-medium tracking-tight hover:text-cool-gray-600 transition duration-150 ease-in-out">
-                  Product
-                  <svg className="w-3.5 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M19 9l-7 7-7-7"></path>
-                  </svg>
-                </a>
-                <a href="#" className="flex items-center justify-center mr-10 text-base text-gray-700 text-opacity-90 font-medium tracking-tight hover:text-cool-gray-600 transition duration-150 ease-in-out">
-                  Community
-                  <svg className="w-3.5 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M19 9l-7 7-7-7"></path>
-                  </svg>
-                </a>
-                <a href="#" className="flex items-center justify-center mr-10 text-base text-gray-700 text-opacity-90 font-medium tracking-tight hover:text-cool-gray-600 transition duration-150 ease-in-out">
-                  Plans
-                </a>
-              </div>
+              <Menu />
             </div>
             <div className="hidden md:flex items-center justify-center">
               <a href="#" className="mr-5 text-lg font-medium text-true-gray-800 hover:text-cool-gray-700 transition duration-150 ease-in-out">learn</a>
